ce275235bde3d8fdd00705af9be35499392c392f
Koen <n12081833@qut.edu.au>
2025-08-11 14:30:56 +1000

    test

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -7,6 +7,6 @@ const router = express.Router();
 router.post('/register', registerUser);
 router.post('/login', loginUser);
 router.get('/profile', protect, getProfile);
-router.put('/profile', protect, updateUserProfile);
+router.put('/profile', protect, updateUserProfile); //test
 
 module.exports = router;
